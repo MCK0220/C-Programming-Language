6011bc4423e69169fbe138be5601f46d9450e453
mck.studios <99533662+MCK0220@users.noreply.github.com>
2022-07-20 00:58:51 +0300

    NumbersAverage
    NumbersAverage

--- NumbersAverage.c
+++ NumbersAverage.c
@@ -2,8 +2,8 @@
 
 	int main()
 	{
-	  int s1,s2,s3,average;
-	  printf("Enter 3 number: ");
+	  int s1,s2,s3,average;    
+	  printf("Enter 3 number: ");  // Please enter three numbers with a space in between.
 	  scanf("%d %d %d",&s1,&s2,&s3);
 
 	  average = (s1+s2+s3)/3;
