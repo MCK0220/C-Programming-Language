ffd9a91f4b10597d5ed5eac6eb259f540b411b92
mck.studios <99533662+MCK0220@users.noreply.github.com>
2022-07-19 20:00:18 +0300

    Create Calculator.c
    How to make Calculator in C

---
+++ Calculator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int number1,number2,total;
+
+    printf("Enter First Number: \t");
+        scanf("%d",&number1);
+
+    printf("Enter Second Number: \t");
+        scanf("%d",&number2);
+
+    total = number1+number2;
+
+    printf("Numbers Total: %d",total);
+}
